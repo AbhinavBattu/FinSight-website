278d274fcd6765980d8618026b7c53abfd448a9a
AbhinavBattu <92840673+AbhinavBattu@users.noreply.github.com>
2024-03-20 13:39:19 +0530

    Added the Project.
    Added the Project.

---
+++ FinSight Website/client/src/App.tsx
@@ -0,0 +1,30 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline } from "@mui/material";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {Box} from "@mui/material";
+import Navbar from "@/scenes/navbar/index";
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions/index";
+function App() {
+  const theme=useMemo(()=> createTheme(themeSettings),[])
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
